fd6515e5a1a586e469bef38470e210d90d048ee6
Michael V <michaelvrld@gmail.com>
2023-12-02 15:00:40 +0100

    Add MaintenanceMode command

---
+++ src/Commands/MaintenanceMode.ts
@@ -0,0 +1,67 @@
+import { BaseCommand, flags } from '@adonisjs/core/build/standalone';
+import { writeFileSync, existsSync, unlinkSync } from 'fs'
+import { DateTime } from 'luxon';
+
+export default class MaintenanceMode extends BaseCommand {
+    /**
+     * Command name is used to run the command
+     */
+    public static commandName = 'maintenance';
+
+    /**
+     * Command description is displayed in the "help" output
+     */
+    public static description = 'Toggle the application maintenance mode';
+
+    public static settings = {
+        loadApp: false,
+        stayAlive: false,
+    }
+
+    @flags.string({ description: 'Secret key to bypass maintenance mode' })
+    public secret: string;
+
+    @flags.string({ description: 'Custom message for maintenance mode' })
+    public message: string;
+
+    public async run() 
+    {
+        if (this.isMaintenanceEnabled())
+            this.disableMaintenanceMode();
+        else
+            this.enableMaintenanceMode();
+    }
+
+    private getMaintenanceFilePath() 
+    {
+        return this.application.tmpPath('maintenance.json');
+    }
+
+    private isMaintenanceEnabled()
+    {
+        return existsSync(this.getMaintenanceFilePath());
+    }
+
+    private enableMaintenanceMode()
+    {
+        writeFileSync(
+            this.getMaintenanceFilePath(),
+            JSON.stringify({ 
+                time: DateTime.now(),
+                secret: this.secret,
+                message: this.message || 'The site is under maintenance.'
+            }), 
+            'utf-8'
+        );
+        this.logger.success('Maintenance mode enabled');
+    }
+    
+    private disableMaintenanceMode()
+    {
+        if (this.isMaintenanceEnabled()) 
+        {
+          unlinkSync(this.getMaintenanceFilePath())
+        }
+        this.logger.success('Maintenance mode disabled');
+    }
+}
